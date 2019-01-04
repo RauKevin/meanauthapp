e88a159b9a2098cf607e2419045891c966633075
RauKevin <kevin_rau87@yahoo.com>
2019-01-04 13:14:00 -0800

    Initail commit + establishment of nodejs backend

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect to mongoDB
+/*
+mongoose.connect(config.database);
+mongoose.connection.on('connected', ()=>{
+    console.log("connected to db");
+});
+mongoose.connection.on('error', (err)=>{
+    console.log("db error: "+err);
+});
+*/
+//var db = mongoose.createConnection(config.uri, config.database, config.port, config.opts);
+/*
+mongoose.connect(config.uri, config.opts, ()=>{
+    console.log('connecting to db...');
+});
+*/
+//{ useNewUrlParser: true }
+mongoose.connect(config.uri, ()=>{
+    console.log('connecting to db...for reals this time');
+});
+mongoose.connection.on('error', (err)=>{ 
+    console.log("db error: "+err);
+});
+
+
+//set up express app
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+// middleware
+app.use(cors());        //to request web assets from another server
+app.use(bodyParser.json());     //stripe off headers?
+app.use(passport.initialize());  //authentication middle ware ?
+app.use(passport.session());  // :____________
+require('./config/passport')(passport);
+app.use('/users', users);
+
+//set Static folder for front end?
+app.use(express.static(path.join(__dirname, 'public')));
+
+//test: get request / index
+app.get('/',(req, res) => {
+    res.send({'myballs':'true'});
+});
+
+//FIX LATER
+app.get('*',(req, res) => {
+    res.send({'myballs':'true'});
+});
+
+app.listen(port, () => {
+    console.log('Sever starting on port '+port);
+});
---
+++ models/users.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+//User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    }, 
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+// Mongoose API wrapper add user to MongoDB
+module.exports.addUser = function(newUser, callback)
+{
+    console.log('0.'+newUser);   //test
+    //generate hash key 
+    bcrypt.genSalt(10, (err, salt)=>{
+        console.log('1.'+newUser);   //test
+        bcrypt.hash(newUser.password, salt, (err, hash)=>{
+            if(err){
+                console.log(err);
+            }
+            newUser.password = hash;
+            console.log('2.'+newUser);   //test
+            newUser.save(callback);
+        });
+    });
+}
+
+
+// Mongoose API wrapper find user by ID
+module.exports.getUserById = function(id, callback)
+{
+    User.findById(id,callback);
+}
+
+// Mongoose API wrapper find first user by username
+module.exports.getUserByUsername = function(username, callback)
+{
+    const query = {username: username};
+    User.findOne(query,callback);
+}
+
+module.exports.comparePassword = function(canidatePassword, hash, callback){
+    console.log('comparing '+canidatePassword+' to '+hash);
+    bcrypt.compare(canidatePassword, hash, (err, isMatch)=>{
+        if(err) throw err;
+        console.log('I guess the password authentication was succesful?');
+        callback(null, isMatch);
+    });
+}
